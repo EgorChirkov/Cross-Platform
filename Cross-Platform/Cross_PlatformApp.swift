709f3ae22ca4665bfebbe9dbf99ad8d8beebcef0
Egor <egorchirkov03@gmail.com>
2022-11-20 17:48:56 +0300

    Initial Commit

---
+++ Cross-Platform/Cross_PlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cross_PlatformApp.swift
+//  Cross-Platform
+//
+//  Created by Егор Чирков on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Cross_PlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
